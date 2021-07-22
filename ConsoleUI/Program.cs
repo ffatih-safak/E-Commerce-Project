219d7b71a6506fa16c91f0bf33779ca764eb9b81
Fatih <ffatihsafak@gmail.com>
2021-07-22 22:02:05 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID Yazılıma Yeni Bir Özeellik Ekliyorsan Hiçbir koda dokunamazsın
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(100,1000))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+            
+        }
+    }
+}
